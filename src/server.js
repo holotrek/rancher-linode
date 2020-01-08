c67e49a076a445eda691673334adf1c0434f676c
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2020-01-08 15:04:07 -0500

    Adds GitHub oauth support

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import http from 'http';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import githubOAuth from 'github-oauth';
+
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+    console.error('GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET environment variables required');
+    process.exit(1);
+}
 
 const app = express();
 const router = express.Router();
@@ -19,11 +25,38 @@ app.use(bodyParser.urlencoded({ extended: true }))
         const pkg = require('../package.json');
         res.send(`<h1>${pkg.name}</h1>
         <h2>Version: ${pkg.version}</h2>`);
+    })
+    .get('/auth', (req, res) => {
+        res.send(
+            `<h1>Login</h1><h2>Click below to login with GitHub Auth</h2><a href="${req.protocol}://${req.hostname}:${req.socket.localPort}/auth/github">Login with GitHub</a>`
+        );
+    })
+    .get('/auth/github', async (req, res) => {
+        console.log('started oauth');
+        return oauth.login(req, res);
+    })
+    .get('/auth/github/callback', async (req, res) => {
+        console.log('received callback');
+        return oauth.callback(req, res);
     });
 
 const port = process.env.NODE_PORT || 3001;
 httpServer.listen(port);
 console.log(`Listening on http://localhost:${port}`);
+
+const oauth = githubOAuth({
+    githubClient: process.env.GITHUB_CLIENT_ID,
+    githubSecret: process.env.GITHUB_CLIENT_SECRET,
+    baseURL: process.env.BASE_URL || `http://localhost:${port}`,
+    loginURI: '/auth/github',
+    callbackURI: '/auth/github/callback'
+});
+oauth.on('error', function(err) {
+    console.error('Login error:', err);
+});
+oauth.on('token', function(token, serverResponse) {
+    serverResponse.end(JSON.stringify(token));
+});
 
 // Setup routes
 router.get('/test', async (req, res) => {
