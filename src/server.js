4f06b32e9450ab34445030b2fafe6c5fa496ac43
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2020-01-10 07:24:01 -0500

    Feature/oauth (#1)
    * Adds GitHub oauth support
    
    * Updates deployment settings and makes it so it infers the Base URL.

--- src/server.js
+++ src/server.js
@@ -3,6 +3,28 @@ import cors from 'cors';
 import http from 'http';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import githubOAuth from 'github-oauth';
+
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+    console.error('GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET environment variables required');
+    process.exit(1);
+}
+const getOAuth = req => {
+    const oauth = githubOAuth({
+        githubClient: process.env.GITHUB_CLIENT_ID,
+        githubSecret: process.env.GITHUB_CLIENT_SECRET,
+        baseURL: `${req.protocol}://${req.hostname}:${req.socket.localPort}`,
+        loginURI: '/auth/github',
+        callbackURI: '/auth/github/callback'
+    });
+    oauth.on('error', function(err) {
+        console.error('Login error:', err);
+    });
+    oauth.on('token', function(token, serverResponse) {
+        serverResponse.end(JSON.stringify(token));
+    });
+    return oauth;
+};
 
 const app = express();
 const router = express.Router();
@@ -19,6 +41,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
         const pkg = require('../package.json');
         res.send(`<h1>${pkg.name}</h1>
         <h2>Version: ${pkg.version}</h2>`);
+    })
+    .get('/auth', (req, res) => {
+        res.send(
+            `<h1>Login</h1><h2>Click below to login with GitHub Auth</h2><a href="${req.protocol}://${req.hostname}:${req.socket.localPort}/auth/github">Login with GitHub</a>`
+        );
+    })
+    .get('/auth/github', async (req, res) => {
+        console.log('started oauth');
+        return getOAuth(req).login(req, res);
+    })
+    .get('/auth/github/callback', async (req, res) => {
+        console.log('received callback');
+        return getOAuth(req).callback(req, res);
     });
 
 const port = process.env.NODE_PORT || 3001;
